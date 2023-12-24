a22b671193b9ad4814e2ac513449c68a09c9ab7f
Ilya Polozkov <154677085+mEINELEEN@users.noreply.github.com>
2023-12-24 16:18:18 +0300

    Update dop2laba.py

--- dop2laba.py
+++ dop2laba.py
@@ -1,18 +1,18 @@
 while True:
     try:
-        def decimal_to_octal(n):
+        def decimal_to_octal(number):
             # Проверяем, является ли число отрицательным
             is_negative = False
-            if n < 0:
+            if number < 0:
                 is_negative = True
-                n = -n
+                number = -number
             # Преобразуем число в восьмеричную систему счисления
             octal = ""
-            while n > 0:
-                octal = str(n % 8) + octal
-                n = n // 8
+            while number > 0:
+                octal = str(number % 8) + octal
+                number = number // 8
             # Если число было отрицательным, применяем дополнительный код
-            if is_negative:
+            if is_negative == True:
                 octal = "1" + octal
 
             return octal
